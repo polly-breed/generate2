82aab0a42f28f31a180c6bde0230842fbda30203
polly-breed <funkyninny@gmail.com>
2024-09-10 13:42:08 +0300

    Revert "Updated documents"
    This reverts commit c209c0af26bf64361264120f80958a1f21d00213.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 
 function generateImages() {
-  const randomImage1 = `https://picsum.photos/1500/1000?random=${Math.floor(Math.random() * 1000)}`;
-  const randomImage2 = `https://picsum.photos/1500/1000?random=${Math.floor(Math.random() * 1000)}`;
+  const randomImage1 = `https://picsum.photos/300/200?random=${Math.floor(Math.random() * 1000)}`;
+  const randomImage2 = `https://picsum.photos/300/200?random=${Math.floor(Math.random() * 1000)}`;
   image1.src = randomImage1;
   image2.src = randomImage2;
 }
