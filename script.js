9f3133d3db414d7c02fbf8864656fdc0a9f1f310
polly-breed <funkyninny@gmail.com>
2024-09-10 14:07:37 +0300

    Revert "Updates: size and picture source"
    This reverts commit 00f6c04c4195b5e774bcdc717fee16c792e354a7.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
+const generateBtn = document.getElementById('generate-btn');
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
-const generateBtn = document.getElementById('generate-btn');
 
 function generateImages() {
-  const randomImage1 = `https://source.unsplash.com/random/800x600?sig=${Math.floor(Math.random() * 1000)}`;
-  const randomImage2 = `https://source.unsplash.com/random/800x600?sig=${Math.floor(Math.random() * 1000)}`;
-  
+  const randomImage1 = `https://picsum.photos/300/200?random=${Math.floor(Math.random() * 1000)}`;
+  const randomImage2 = `https://picsum.photos/300/200?random=${Math.floor(Math.random() * 1000)}`;
   image1.src = randomImage1;
   image2.src = randomImage2;
 }
